fd2c29df2a1c86f7f4d1fbec6f952cd24e2517cc
Eugen1o <pendiur.zenia@gmail.com>
2024-09-20 11:08:25 +0200

    Eugenio

--- untitled/src/Entrada.java
+++ untitled/src/Entrada.java
@@ -1,2 +1,45 @@
 public class Entrada {
+
+    static int edadClace = 50;
+
+    // mod_acc mod_adinal mod_ret nombrre (argumentos) { cuerpo }
+
+        public static void main (String[] args){
+
+            System.out.println("Proyecto de variables");
+
+            // Tipos nombre = valor (el valor es opicional)
+            final String CIF = "812345"; // la variable final se escribe todo en mayuscula
+            char letra = 'A';
+
+            short numeroPequenio = 23; // numeros pequepo (-255,255)
+            int edad = 18; // numeros sin decimal (-65.435, 65.435)
+            System.out.println("Mi edad es "+edad);
+            edad = 18;
+            System.out.println("Cambiando edad");
+            System.out.println("Mi edad es "+edad);
+            long telefono = 123456789;
+            Long telefonoCompleto;
+
+            float altura = 3.14f;
+            Float alturaCompleta;
+            double dessimalesDetalle = 3.1415163456;
+            boolean carne = true; // false
+    }
+
+
+
+    public static void miMetodo (){
+
+           String nombre = "Julia";
+           int edad = 40;
+           edadClace=30;
+            System.out.println(edadClace);
+           //this.edadClace =90;
+
+    }
+        public static void otroMetodo(){
+
+            }
+
 }
